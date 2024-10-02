ab0466fe3fa12bca5e773436d730e32da99caa89
Rafael Mendes <rafael1jmendes@gmail.com>
2024-10-02 13:35:33 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse e o Sreen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2022;
+        System.out.println("Ano de lancamento:" +ano);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        //media calculada por 3 pessoas
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = "Filme de aventura com gala dos anos 80";
+        System.out.println(sinopse);
+
+        int classificacao;
+        classificacao = (int) (media /2);
+        System.out.println(classificacao);
+    }
+}
